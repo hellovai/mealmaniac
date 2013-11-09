65317c7dd270fc9fb02ad4f3a55998bfc99b6ac4
Vaibhav Gupta <kacxdak@gmail.com>
2013-11-09 14:16:43 -0500

    changed port

--- server-py/main.py
+++ server-py/main.py
@@ -218,4 +218,4 @@ def getMeal(uid, price, nick):
 	return json.dumps({"core":meals, "total": spent * 1.0875 * (1 + float(user["tip"])/100) })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=8080)
